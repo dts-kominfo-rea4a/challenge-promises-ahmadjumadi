c3c2a395425e457c64136b789cb3bc4ef9cb6688
Ahmad Jumadi <ahmadjumadi@smkn1karawang.sch.id>
2022-09-15 22:57:01 +0700

    test dulu

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = () => {
+  return new Promise((resolve, reject) => {
+    resolve(promiseTheaterIXX());
+    reject(promiseTheaterVGC());
+  });
+};
 
 module.exports = {
   promiseOutput,
