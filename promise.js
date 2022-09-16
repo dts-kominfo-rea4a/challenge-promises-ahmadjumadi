96538fc31a10d0811d995a1184b1ee3b78fcec3a
Ahmad Jumadi <ahmadjumadi@smkn1karawang.sch.id>
2022-09-16 17:00:58 +0700

    hasil tugas insya allah oke

--- promise.js
+++ promise.js
@@ -1,11 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = () => {
-  return new Promise((resolve, reject) => {
-    resolve(promiseTheaterIXX());
-    reject(promiseTheaterVGC());
-  });
+const promiseOutput = async (emosi) => {
+  let jumlahEmosi = 0;
+  const teaterIXX = await promiseTheaterIXX();
+  const teaterVGC = await promiseTheaterVGC();
+  //gabung teaterIXX & teaterVGC to teater
+  const teater    = teaterIXX.concat(teaterVGC);
+
+  //looping teater dan ambil jumlahEmosi
+  for (let i=0; i<teater.length; i++){
+    if (teater[i].hasil == emosi){
+        jumlahEmosi++;
+    }     
+  }
+  //kemablikan nilai jumlahEmosi
+  return jumlahEmosi;
 };
 
 module.exports = {
